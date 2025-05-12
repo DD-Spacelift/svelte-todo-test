e17e02fede66735f001a224ca87084e090c185c1
Stefan Marx <stefan@marx-merten.de>
2025-05-12 15:45:37 +0200

    added basic list interface

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,22 @@
-<div class="hero bg-base-200 min-h-screen">
-    <div class="hero-content flex-col lg:flex-row">
-      <img
-        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-        class="max-w-sm rounded-lg shadow-2xl"
-      />
-      <div>
-        <h1 class="text-5xl font-bold">DD Todo demo app!</h1>
-        <p class="py-6">
-          Here you can find the best Todo apps in demo space, ever created.
-        </p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
+<script lang="ts">
+    import TodoLists from '$lib/components/TodoLists.svelte';
+</script>
+
+<div class="bg-base-200">
+    <div class="container mx-auto px-4 py-8">
+        <div class="flex flex-col md:flex-row items-center gap-8">
+            <img
+                src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                class="w-48 rounded-lg shadow-xl"
+            />
+            <div class="text-center md:text-left">
+                <h1 class="text-4xl font-bold">DD Todo demo app!</h1>
+                <p class="mt-2">
+                    Here you can find the best Todo apps in demo space, ever created.
+                </p>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
+
+<TodoLists />
